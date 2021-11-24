461f38c83a622ed84726fb50d4bace4c1125adc9
Christian Hussmann <chri.hussmann@gmail.com>
2021-11-24 10:28:58 +0100

    Create GroupsDto.cs

---
+++ TitanPass.PasswordManager.WebApi/Dtos/GroupsDto.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace TitanPass.PasswordManager.WebApi.Dtos
+{
+    public class GroupsDto
+    {
+        private List<GroupDto> Groups { get; set; }
+    }
+}
